19e7a5f706edfd15241c14950551a330db409d3e
yesh20hiya-afk <yesh20hiya@gmail.com>
2025-12-18 20:29:24 -0500

    v1

---
+++ project1.py
@@ -0,0 +1,15 @@
+# this is raj's friends name Bob charlie dave garret harry
+# They all have random birthday dates
+
+Bob= "9/18/15"
+charlie= "3/22/14"
+dave= "11/5/13"
+garret= "7/30/16"
+harry= "1/12/15"
+print("Bob's birthday is on:", Bob)
+print("Charlie's birthday is on:", charlie)
+print("Dave's birthday is on:", dave)
+print("Garret's birthday is on:", garret)
+print("Harry's birthday is on:", harry)
+
+# End of the code
